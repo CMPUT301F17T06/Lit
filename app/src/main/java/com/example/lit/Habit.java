c6ea9680345fc763946914861f12c759525db7f0
weikailu <weikailu96@gmail.com>
2017-10-20 21:36:58 -0600

    habit class/interface v1

--- app/src/main/java/com/example/lit/Habit.java
+++ app/src/main/java/com/example/lit/Habit.java
@@ -1,8 +1,46 @@
 package com.example.lit;
 
+import java.util.Date;
+
 /**
  * Created by damon on 10/20/2017.
  */
 
-public class Habit {
+public abstract class Habit implements HabitAddable{
+
+    private String habitName;
+    private Date date = new Date();
+
+    public Habit(String habitName, Date date) {
+        this.habitName = habitName;
+        this.date = date;
+    }
+
+    public Habit(String habitName) {
+        this.habitName = habitName;
+    }
+
+    public String getHabitName() {
+        return habitName;
+    }
+
+    public void setHabitName(String habitName) {
+        this.habitName = habitName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Habit{" +
+                "habitName='" + habitName + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
---
+++ app/src/main/java/com/example/lit/HabitAddable.java
@@ -0,0 +1,14 @@
+package com.example.lit;
+
+/**
+ * Created by weikailu on 2017-10-20.
+ */
+
+import java.util.Date;
+
+public interface HabitAddable {
+
+    public String getHabitName();
+
+    public Date  getDate();
+}
