2672073bf4cdabf0e6694cad209c291c947ef70a
amahdi93 <amahdi@ualberta.ca>
2017-10-21 14:03:44 -0600

    added perliminary habit event test

---
+++ app/src/androidTest/java/com/example/lit/HabitEventTest.java
@@ -0,0 +1,17 @@
+package com.example.lit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by ammar on 21/10/17.
+ */
+
+public class HabitEventTest extends ActivityInstrumentationTestCase2 {
+    public HabitEventTest(){
+        super(com.example.lit.HabitEvent.class);
+    }
+
+    public void testAddHabitEvent(){
+    }
+}
+
