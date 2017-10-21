bbacd83d6fe4177f9134391ea1b4ab0b79823f79
weikailu <weikailu96@gmail.com>
2017-10-20 21:54:35 -0600

    modified unit test file

---
+++ app/src/androidTest/java/com/example/lit/HabitTest.java
@@ -0,0 +1,19 @@
+package com.example.lit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by weikailu on 2017-10-20.
+ */
+
+public class HabitTest extends  ActivityInstrumentationTestCase2{
+    public HabitTest(){
+        super(com.example.lit.Habit.class);
+    }
+
+    public void testAddHabit(){
+        //
+    }
+
+    //
+}
