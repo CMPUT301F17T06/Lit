02104963177d89aebad1bd8d6cf76564743d2f48
Damon <jiaxiong@ualberta.ca>
2017-10-21 00:42:28 -0600

    Build the location model class and LocationTest file

---
+++ app/src/androidTest/java/com/example/lit/LocationTest.java
@@ -0,0 +1,16 @@
+package com.example.lit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by damon on 10/21/2017.
+ */
+
+public class LocationTest extends ActivityInstrumentationTestCase2 {
+    public LocationTest() {
+        super(com.example.lit.Habit.class);
+    }
+
+
+}
+
--- app/src/main/java/com/example/lit/Location.java
+++ app/src/main/java/com/example/lit/Location.java
@@ -3,6 +3,19 @@ package com.example.lit;
 /**
  * Created by damon on 10/20/2017.
  */
-
+import com.google.android.gms.maps.model.LatLng;
 public class Location {
+    private LatLng location;
+
+    public Location (LatLng location){
+        this.location = location;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
+    }
 }
