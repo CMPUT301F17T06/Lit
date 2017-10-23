7c091b8ba1a78359e90ed3500b5148a1479b1bf6
RileyMKDixon <rmdixon@ualberta.ca>
2017-10-23 11:13:03 -0600

    Fix bug
    When the interface name was changed, Habit.java was not updated to
    implement the new name.

--- app/src/main/java/com/example/lit/habit/Habit.java
+++ app/src/main/java/com/example/lit/habit/Habit.java
@@ -20,7 +20,7 @@ import io.searchbox.annotations.JestId;
  * Created by weikailu on 10/20/2017.
  */
 
-public abstract class Habit implements HabitAddable{
+public abstract class Habit implements Habitable{
 
     private String title;
     private Date date;
