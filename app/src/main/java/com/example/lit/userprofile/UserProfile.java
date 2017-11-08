0b9c61b3f5d05b7df556163bc12d4ce7a5c71bb8
Damon <jiaxiong@ualberta.ca>
2017-11-08 13:29:13 -0700

    completed the function in userprofile, create a login page layout

--- app/src/main/java/com/example/lit/userprofile/UserProfile.java
+++ app/src/main/java/com/example/lit/userprofile/UserProfile.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 /**
  * Created by Riley Dixon on 21/10/2017.
+ * edited by Damon on 08/11/2017
  */
 
 public abstract class UserProfile {
@@ -22,11 +23,10 @@ public abstract class UserProfile {
     //is that of the current user
     //private ArrayList<String> followedUsers; Depending on how we manage this
 
-    public UserProfile() { //Dummy Constructor, sets nullable.
 
-    }
 
     public UserProfile(String name){ //New Account
+        this.name = name;
 
     }
 
@@ -34,91 +34,109 @@ public abstract class UserProfile {
                        Bitmap profileImage, ArrayList<Habit> currentHabits,
                        ArrayList<HabitEvent> habitCompletionHistory,
                        ArrayList<String> followingUsers){
+        this.name = name;
+        this. profileDescription = profileDescription;
+        this.profileImage = profileImage;
+        this.currentHabits = currentHabits;
+        this.habitCompletionHistory = habitCompletionHistory;
+        this.followingUsers = followingUsers;
 
     }
 
     public void setName(String name){
-
+        this.name = name;
     }
 
     public String getName(){
-
+        return name;
     }
 
-    public void setProfileDescription(String name){
-
+    public void setProfileDescription(String profileDescription){
+        this.profileDescription = profileDescription;
     }
 
     public String getProfileDecription(){
-
+        return profileDescription;
     }
 
-    public void setProfileImage(Bitmap image){
-
+    public void setProfileImage(Bitmap profileImage){
+        this.profileImage = profileImage;
     }
 
     public Bitmap getProfileImage(){
-
+        return profileImage;
     }
 
     public void setCurrentHabits(ArrayList<Habit> currentHabits){
-
+        this.currentHabits = currentHabits;
     }
 
     public ArrayList<Habit> getCurrentHabits(){
-
+        return currentHabits;
     }
 
     public Habit getHabit(int habitPosition){
-
+        return currentHabits.get(habitPosition);
     }
 
     public void setHabitCompletionHistory(
             ArrayList<HabitEvent> habitCompletionHistory){
-
+        this.habitCompletionHistory = habitCompletionHistory;
     }
 
     public ArrayList<HabitEvent> getHabitCompletionHistory(){
-
+        return habitCompletionHistory;
     }
 
     public HabitEvent getHabitEvent(int eventPosititon){
-
+        return habitCompletionHistory.get(eventPosititon);
     }
 
     public void setFollowingUsers(
             ArrayList<String> followingUsers){
-
+        this.followingUsers = followingUsers;
     }
 
     public ArrayList<String> getFollowingUsers(){
-
+        return followingUsers;
     }
 
     public String getFollowingUser(int userPosition){
-
+        return followingUsers.get(userPosition);
     }
 
     public void addHabit(Habit newHabit){
-
+        currentHabits.add(newHabit);
     }
 
     public void deleteHabit(Habit deletedHabit){
-
+        currentHabits.remove(deletedHabit);
     }
 
     public void addHabitEvent(HabitEvent newHabitEvent){
-
+        habitCompletionHistory.add(newHabitEvent);
     }
 
     public Boolean addFollowingUser(String name){
+        if (followingUsers.contains(name)) {
+            return false;
+        }
+        else{
+            followingUsers.add(name);
+            return true;}
 
     }
     //False if the user is already following
     //True if the user added successfully
 
-    public Boolean removeFollowingUser(){
-
+    public Boolean removeFollowingUser(String name){
+        if(followingUsers.contains(name)){
+            followingUsers.remove(name);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 
