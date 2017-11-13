3ab3301ffddbd9c255b4ea15fdd165265deb3bf2
RileyMKDixon <rmdixon@ualberta.ca>
2017-11-13 01:49:05 -0700

    Implement generics to ensure proper typing of objects
    This one took a while. There is still a lot of work to do however the
    large issue of ensuring that code is type safe at run time has been
    solved after a quick refresher in Java generics.

--- app/src/main/java/com/example/lit/saving/DataHandler.java
+++ app/src/main/java/com/example/lit/saving/DataHandler.java
@@ -17,6 +17,7 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -26,20 +27,70 @@ import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+
 /**
- * Created by Riley Dixon on 12/11/2017.
+ * The DataHandler class is to be used to save an object for longtime storage.
+ * This object should be used for a single object or a collection of the same type
+ * of objects.
+ * Objects that are not the same type should be separated into two different
+ * DataHandler objects and as such two different files.
+ *
+ * @author Riley Dixon
  */
 
-public class DataHandler {
+//TODO: BONUS, upgrade to java.nio.files.Files and introduce file locking
+    //The above TO DO is just a bonus and should be used to ensure
+    //file consistency but ensuring that there is only one writer or reader
+    // at a time.
+
+public class DataHandler<T> {
     private long lastOfflineSave;
     private long lastOnlineSave;
-    private String filename;
+    private String FILENAME;
+    private Type typeOfDataBeingStored;
 
-    DataHandler(String username){
-        this.filename = username;
+    /**
+     * Builds a handler that is used to save data to both local storage for offline use as
+     * well as online via ElasticSearch.
+     *
+     * @param username What the username of the current logged in user is.
+     *                 This is required to give a cleaner file hierarchy.
+     * @param savedObject What object is currently being saved? Is this HabitList or
+     *                    HabitHistory for example.
+     * @param context The context of the activity. In most cases when constructing
+     *                the object this parameter is "this".
+     * TODO: Fix Javadoc to clarify that the below parameter is the generic T and no longer a parameter
+     * @param typeOfDataBeingStored The type of data being stored. As arrayList's
+     *                              are being used and we cannot confirm what the
+     *                              type of the arrayList is at runtime due to Java's
+     *                              type erasure of generics at runtime, the type of
+     *                              arrayList being passed MUST be passed explicitly.
+     *                              See the GSON Javadoc for how to generate the Type
+     *                              object to pass to this constructor.
+     *
+     * @see Gson
+     */
+    DataHandler(String username, String savedObject, Context context, Type typeOfDataBeingStored){
+        this.FILENAME = context.getFilesDir().getAbsolutePath() + File.separator
+                + username;
+
+        File filePath = new File(FILENAME);
+        //Check if the subdirectory has been created yet or not
+        //Create subdirectory if it hasn't been created yet.
+        if(!filePath.exists()){
+            filePath.mkdirs();
+        }
+
+        FILENAME += File.separator + savedObject + ".sav";
+        this.typeOfDataBeingStored = typeOfDataBeingStored;
+
     }
 
-    public void saveArrayList(ArrayList<?> arrayList){
+    public void setTypeOfDataBeingStored(Type typeOfDataBeingStored){
+        this.typeOfDataBeingStored = typeOfDataBeingStored;
+    }
+
+    public void saveArrayList(ArrayList<T> arrayListToBeSaved){
 
     }
 
@@ -47,7 +98,7 @@ public class DataHandler {
         
     }
 
-    public ArrayList<?> loadArrayList(){
+    public void loadArrayList(ArrayList<T> arrayListToBeLoaded){
 
     }
 
@@ -55,17 +106,16 @@ public class DataHandler {
 
     }
 
-    private saveArrayToOffline(){
+    private void saveArrayToOffline(ArrayList<T> arrayListToSave){
         try {
-            FileOutputStream fos = openFileOutput(FILENAME,
-                    Context.MODE_PRIVATE);
-
+            FileOutputStream fos = new FileOutputStream(new File(FILENAME));
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
-
+            Type typeOfArrayList = new TypeToken<ArrayList<T>>(){}.getType();
+            //Write to the stream.
             Gson gson = new Gson();
-            gson.toJson(counterArrayList, out);
+            gson.toJson(arrayListToSave, typeOfArrayList, out); //OBJECT, TYPE, APPENDABLE
+            //Close the writing stream.
             out.flush();
-
             fos.close();
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
@@ -76,48 +126,45 @@ public class DataHandler {
         }
     }
 
-    private saveArrayToOnline(){
+    private void saveArrayToOnline(){
 
     }
 
-    private saveSingularElementToOffline(){
+    private void saveSingularElementToOffline(){
 
     }
 
-    private saveSingularElementToOnline(){
+    private void saveSingularElementToOnline(){
 
     }
 
-    private loadArrayToOffline(){
+    private ArrayList<T> loadArrayToOffline(ArrayList<T> arrayListToLoad){
         try {
-            FileInputStream fis = openFileInput(FILENAME);
+            FileInputStream fis = new FileInputStream(new File(FILENAME));
             BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Type typeOfArrayList = new TypeToken<ArrayList<T>>(){}.getType();
 
             Gson gson = new Gson();
-
-            //Taken from https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
-            // 2017-09-19
-            Type listType = new TypeToken<ArrayList<Counter>>(){}.getType();
-            counterArrayList = gson.fromJson(in, listType);
+            ArrayList<T> arrayList = gson.fromJson(in, typeOfArrayList);
 
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
-            counterArrayList = new ArrayList<Counter>();
+            counterArrayList = new ArrayList<T>();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             throw new RuntimeException();
         }
     }
 
-    private loadArrayToOffline(){
+    private void loadArrayToOffline(){
 
     }
 
-    private loadSingularElementToOffline(){
+    private void loadSingularElementToOffline(){
 
     }
 
-    private loadSingularElementToOnline(){
+    private void loadSingularElementToOnline(){
 
     }
 
