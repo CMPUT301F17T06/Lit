f66aa3198341f335481134e95122ed9c23436e55
RileyMKDixon <rmdixon@ualberta.ca>
2017-11-12 19:51:07 -0700

    Start working on DataHandler

--- app/src/main/java/com/example/lit/saving/DataHandler.java
+++ app/src/main/java/com/example/lit/saving/DataHandler.java
@@ -10,6 +10,20 @@
 
 package com.example.lit.saving;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 /**
@@ -19,13 +33,92 @@ import java.util.ArrayList;
 public class DataHandler {
     private long lastOfflineSave;
     private long lastOnlineSave;
+    private String filename;
+
+    DataHandler(String username){
+        this.filename = username;
+    }
 
     public void saveArrayList(ArrayList<?> arrayList){
 
     }
 
-    public void saveSingularElement(<?> element){
+    public void saveSingularElement(Object element){
         
+    }
+
+    public ArrayList<?> loadArrayList(){
+
+    }
+
+    public Object loadSingularElement(){
+
+    }
+
+    private saveArrayToOffline(){
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(counterArrayList, out);
+            out.flush();
+
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+
+    private saveArrayToOnline(){
+
+    }
+
+    private saveSingularElementToOffline(){
+
+    }
+
+    private saveSingularElementToOnline(){
+
+    }
+
+    private loadArrayToOffline(){
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            //Taken from https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+            // 2017-09-19
+            Type listType = new TypeToken<ArrayList<Counter>>(){}.getType();
+            counterArrayList = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            counterArrayList = new ArrayList<Counter>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+
+    private loadArrayToOffline(){
+
+    }
+
+    private loadSingularElementToOffline(){
+
+    }
+
+    private loadSingularElementToOnline(){
+
     }
 
 }
