f1a91c0e2c9d4c00dfdb89a1f1ae9aa21cbf528f
footinput <mwschafe@ualberta.ca>
2017-11-18 22:45:59 -0700

    NavigationDrawerAdded

--- app/src/main/java/com/example/lit/activity/HomePageActivity.java
+++ app/src/main/java/com/example/lit/activity/HomePageActivity.java
@@ -13,9 +13,14 @@ package com.example.lit.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.location.Location;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,6 +30,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lit.R;
 import com.example.lit.exception.HabitFormatException;
@@ -98,13 +104,21 @@ public class HomePageActivity extends AppCompatActivity {
     private double lat;
     private double lng;
 
+    //initializing navigation drawer
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-        addHabitButton = (ImageButton) findViewById(R.id.AddHabit);
+        setContentView(R.layout.activity_home_page_updated);
+        //OLD CODE COMMENTED OUT
+        /*addHabitButton = (ImageButton) findViewById(R.id.AddHabit);
         Maps = (Button) findViewById(R.id.Maps) ;
         HabitHistory =  (Button) findViewById(R.id.HabitHistory) ;
         Friends  = (Button) findViewById(R.id.Friend);
@@ -178,8 +192,92 @@ public class HomePageActivity extends AppCompatActivity {
                 intent.putExtras(bundle);
                 startActivityForResult(intent,2);
             }
+        });*/
+        //NEW CODE BELOW
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "HabitHistory", "Friends", "Map", "Profile" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(HomePageActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onStart() {
         // TODO Auto-generated method stub
@@ -187,9 +285,9 @@ public class HomePageActivity extends AppCompatActivity {
         loadFromFile();
 
 
-        habitAdapter = new ArrayAdapter<Habit>(this,
+       /* habitAdapter = new ArrayAdapter<Habit>(this,
                 R.layout.list_item, habitArrayList);
-        habitsListView.setAdapter(habitAdapter);
+        habitsListView.setAdapter(habitAdapter);*/
 
     }
     /**
