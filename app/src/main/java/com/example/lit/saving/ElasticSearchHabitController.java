3bafc1811c102338e05e672a40f2042a4792aafa
footinput <mwschafe@ualberta.ca>
2017-12-03 19:05:06 -0700

    ElasticSearchHabitController updated

--- app/src/main/java/com/example/lit/saving/ElasticSearchHabitController.java
+++ app/src/main/java/com/example/lit/saving/ElasticSearchHabitController.java
@@ -12,9 +12,14 @@ package com.example.lit.saving;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.lit.habit.NormalHabit;
+import com.example.lit.userprofile.UserProfile;
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
@@ -31,8 +36,151 @@ import io.searchbox.core.SearchResult;
  * @author Max Schafer, modified by Riley Dixon
  * @see DataHandler
  */
-class ElasticSearchHabitController {
+public class ElasticSearchHabitController {
      private static JestDroidClient client;
+
+    // Add Habit to elastic search
+    public static class AddHabitsTask extends AsyncTask<NormalHabit, Void, Void> {
+
+        @Override
+        protected Void doInBackground(NormalHabit... habits) {
+            verifySettings();
+
+            for (NormalHabit habit : habits) {
+                Index index = new Index.Builder(habit).index("cmput301f17t06").type("habit").build();
+
+                try {
+
+                    DocumentResult result = client.execute(index);
+
+                    if(result.isSucceeded())
+                    {
+                        habit.setID(result.getId());
+                    }
+                    else
+                    {
+                        Log.i("Error","Elasticsearch was not able to add the habit");
+                    }
+                }
+                catch (Exception e) {
+                    Log.i("Error", "The application failed to build and send the habits");
+                }
+
+            }
+            return null;
+        }
+    }
+
+    // Gets all habits for the user specified
+    public static class GetCurrentHabitsTask extends AsyncTask<String, Void, ArrayList<NormalHabit>> {
+        @Override
+        protected ArrayList<NormalHabit> doInBackground(String... search_parameters) {
+            verifySettings();
+
+            ArrayList<NormalHabit> habits;
+            habits = new ArrayList<NormalHabit>();
+
+
+            String query = "{\n" +
+                    "    \"query\" : {\n" +
+                    "       \"constant_score\" : {\n" +
+                    "           \"filter\" : {\n" +
+                    "               \"term\" : {\"user\": \"" + search_parameters[0] + "\"}\n" +
+                    "             }\n" +
+                    "         }\n" +
+                    "    }\n" +
+                    "}";
+            Search search = new Search.Builder(query).addIndex("cmput301f17t06").addType("habit").build();
+
+
+            try {
+                // get the results of the query
+                SearchResult result = client.execute(search);
+                if(result.isSucceeded())
+                {
+                    List<NormalHabit> foundHabit = result.getSourceAsObjectList(NormalHabit.class);
+                    habits.addAll(foundHabit);
+                }
+            }
+            catch (Exception e) {
+                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+            }
+
+            return habits;
+        }
+    }
+
+    // Add new user to elastic search
+    public static class AddUserTask extends AsyncTask<UserProfile, Void, Void> {
+
+        @Override
+        protected Void doInBackground(UserProfile... userProfiles) {
+            verifySettings();
+
+            //TODO fix Mapping parse error document is full.
+            for (UserProfile userProfile : userProfiles) {
+                Index index = new Index.Builder(userProfile).index("cmput301f17t06").type("user").build();
+
+                try {
+
+                    DocumentResult result = client.execute(index);
+
+                    if(result.isSucceeded())
+                    {
+                        userProfile.setID(result.getId());
+                    }
+                    else
+                    {
+                        Log.i("Error","Elasticsearch was not able to add the habit");
+                    }
+                }
+                catch (Exception e) {
+                    Log.i("Error", "The application failed to build and send the habits");
+                }
+
+            }
+            return null;
+        }
+    }
+
+    // Get user from elastic search
+    public static class GetUserTask extends AsyncTask<String, Void, UserProfile> {
+        private UserProfile userProfile = null;
+
+        @Override
+        protected UserProfile doInBackground(String... search_parameters) {
+            verifySettings();
+
+            String query = "{\n" +
+                    "    \"query\" : {\n" +
+                    "       \"constant_score\" : {\n" +
+                    "           \"filter\" : {\n" +
+                    "               \"term\" : {\"name\": \"" + search_parameters[0] + "\"}\n" +
+                    "             }\n" +
+                    "         }\n" +
+                    "    }\n" +
+                    "}";
+
+            Search search = new Search.Builder(query).addIndex("cmput301f17t06").addType("user").build();
+
+            try {
+                // get the results of the query
+                SearchResult result = client.execute(search);
+                if(result.isSucceeded() && result.getFirstHit(UserProfile.class).source != null)
+                {
+                    userProfile = result.getFirstHit(UserProfile.class).source;
+                    String blah = "p";
+                } else {
+                    Log.i("Error", "The search query failed to find any Users that matched");
+                }
+            }
+            catch (Exception e) {
+                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+            }
+
+            return userProfile;
+        }
+    }
 
      static class AddTask<T extends Saveable> extends AsyncTask<ElasticSearchTimestampWrapper<T>, Void, Void> {
         private String username;
